40001c1444a40f308d60dade065c45519f223f7a
dawei <dawei@connect.hku.hk>
2021-12-02 20:40:43 +0800

    ma_03

--- examples/exp_configs/rl/multiagent/multiagent_ring.py
+++ examples/exp_configs/rl/multiagent/multiagent_ring.py
@@ -74,7 +74,7 @@ flow_params = dict(
     # environment related parameters (see flow.core.params.EnvParams)
     env=EnvParams(
         horizon=HORIZON,
-        warmup_steps=750,
+        warmup_steps=100,
         clip_actions=False,
         additional_params={
             "max_accel": 1,
